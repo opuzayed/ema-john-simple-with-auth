af1ca5b5a8e460a29874e725eb65f4004d382f29
opuzayed <zayedopu4933@gmail.com>
2022-10-19 16:40:02 +0600

    navigate use useNavigate

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/UserContext";
 import logo from "../../images/Logo.svg";
 import "./Header.css";
 
 const Header = () => {
-    
+    const {user, logOut} = useContext(AuthContext);
   return (
     <nav className="header">
       <img src={logo} alt="" />
@@ -12,9 +14,16 @@ const Header = () => {
         <Link to="/orders">Orders</Link>
         <Link to="/inventory">Inventory</Link>
         <Link to="/about">About</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign up</Link>
-       
+        {
+            user?.uid ?
+            <button className='btn-logOut' onClick={logOut}>Log Out</button>
+            :
+            <>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Sign up</Link>
+            </>
+            
+        }
       </div>
     </nav>
   );
