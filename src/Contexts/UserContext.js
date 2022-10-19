4a5388995bca448090e2a4a44f460337de47867b
opuzayed <zayedopu4933@gmail.com>
2022-10-19 21:48:17 +0600

    exact show route when loading

--- src/Contexts/UserContext.js
+++ src/Contexts/UserContext.js
@@ -7,6 +7,7 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 const UserContext = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
  
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password)
@@ -24,13 +25,14 @@ const UserContext = ({ children }) => {
   const unSubscribe = onAuthStateChanged( auth, currentUser => {
       console.log('current user :', currentUser);
       setUser(currentUser);
+      setLoading(false);
     });
 
     return () => unSubscribe();
 
   },[]);
 
-  const authInfo = { user, createUser, signIn, logOut };
+  const authInfo = { user, loading, createUser, signIn, logOut };
 
   return (
     <AuthContext.Provider value={authInfo}>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/UserContext';
 import './Login.css';
 const Login = () => {
     const {signIn} = useContext(AuthContext);
     const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state.from.path || '/';
+
     const handleSubmit = event => {
         event.preventDefault();
         const form = event.target;
@@ -16,7 +19,7 @@ const Login = () => {
             const user = result.user;
             console.log(user);
             form.reset();
-            navigate('/');
+            navigate(from, {replace : true});
         })
         .catch(error => console.error(error));
     }
